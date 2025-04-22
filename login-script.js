7234ca5476965dcdf98f27c46d44b7f3ed5a52c1
Jesse <jesseamundsen@gmail.com>
2025-04-22 19:45:23 -0400

    fix small stuff

--- login-script.js
+++ login-script.js
@@ -1,12 +1,12 @@
 function init() {
 
     $('#login-button').click(function() {
-        Cookies.set('outsidely','Basic ' + btoa($('#userid').val() + ':' + $('#password').val()));
+        Cookies.set('outsidely','Basic ' + btoa($('#userid').val() + ':' + $('#password').val()), { expires: 30 });
         window.location.href = 'index.html';
     });
 
     $('#logout-button').click(function() {
-        Cookies.set('outsidely','');
+        Cookies.remove('outsidely');
         window.location.href = 'index.html';
     });
 
